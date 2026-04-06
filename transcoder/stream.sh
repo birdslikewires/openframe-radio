9c55f10573ce2f83607b33cd77bda7ff7ce112a3
Andrew Davison <andydvsn@users.noreply.github.com>
2026-04-06 03:10:17 +0100

    Update stream.sh

--- transcoder/stream.sh
+++ transcoder/stream.sh
@@ -16,10 +16,6 @@ free_tuners() {
     curl -sf "http://$HDHRIP/tuners.html" | grep -c "not in use"
 }
 
-channel_name() {
-    curl -sf "http://$HDHRIP/lineup.json" \
-        | jq -r --arg ch "$CHANNEL" '.[] | select(.GuideNumber == $ch) | .GuideName' 2>/dev/null
-}
 
 icecast_has_mount() {
     curl -sf "http://localhost:8000/status-json.xsl" | python3 -c "
@@ -42,15 +38,11 @@ fi
 
     if ! icecast_has_mount; then
         [[ "$CHANNEL" -ge 700 ]] && BUFFER=24000 || BUFFER=48000
-        STREAM_NAME=$(channel_name)
-        ICECAST_URL="icecast://source:$ICECAST_PASS@localhost:8000/$CHANNEL"
-        [ -n "$STREAM_NAME" ] && ICECAST_URL+="?ice_name=${STREAM_NAME// /+}"
-
         ffmpeg -hide_banner -loglevel error \
             -probesize "$BUFFER" \
             -i "http://$HDHRIP:5004/auto/v$CHANNEL" \
             -vn -acodec libmp3lame -b:a 128k -f mp3 \
-            "$ICECAST_URL" \
+            "icecast://source:$ICECAST_PASS@localhost:8000/$CHANNEL" \
             </dev/null >/dev/null 2>&1 9>&- &
 
         echo $! > "$STREAMDIR/$CHANNEL.pid"
